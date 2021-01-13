5e8bf086da2aff0a4997bce9c2662892825a3605
CodingMania <vasilmitovv@gmail.com>
2021-01-13 15:18:13 +0200

    Initial commit

---
+++ JS Back-End - януари 2021/Cat Shelter/index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = 3000;
+
+http.createServer((req, res)=> {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.write('Hello JS World');
+    res.end();
+
+}).listen(port, ()=> console.log(`Server is listening on ${port}...`))
