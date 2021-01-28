6e1cff0a17208a83aa56444922d15e697a1a35bb
CodingMania <vasilmitovv@gmail.com>
2021-01-28 17:31:30 +0200

    Implement searching

--- JS Back-End - януари 2021/Cubicle-Workshop-Part-1/controllers/productController.js
+++ JS Back-End - януари 2021/Cubicle-Workshop-Part-1/controllers/productController.js
@@ -5,6 +5,21 @@ const productService = require('../services/productService');
 
 router.get('/', (req, res) => {
     let products = productService.getAll();
+
+    let { search, from, to } = req.query;
+    
+    if(search){
+        products = products.filter(x => x.name.toLowerCase().includes(search));
+    }
+
+    if(from){
+        products = products.filter(x => +x.level >= from )
+    }
+
+    if(to){
+        products = products.filter(x => +x.level <= to )
+    }
+
     res.render('index', {title: 'Home', products});
 });
 
